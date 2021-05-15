d1580032f8c57aec8c031b1ae6659497de6095af
monaseyf <monaseyf25.com>
2021-05-15 06:56:18 +0430

    first commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,190 @@
+<template>
+   <v-row >
+       <v-col class="col-lg-3">
+          <v-card
+            width="256"
+            height="100vh"
+            tile>
+      <v-navigation-drawer permanent>
+        <v-system-bar></v-system-bar>
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+  
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                sysadmin
+              </v-list-item-title>
+              <v-list-item-subtitle>admin@email.com</v-list-item-subtitle>
+            </v-list-item-content>
+  
+            <v-list-item-action>
+              <v-icon>mdi-menu-down</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="selectedItem"
+            color="primary"
+          >
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+            >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+  
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+       </v-col>
+       <v-col class="col-lg-9">
+           <v-card>
+               <v-card-title>َAdmin Dashboard</v-card-title>
+               <v-row>
+                   <v-col class="col-lg-12">
+                         <v-data-table
+                            :headers="headers"
+                            :items="countries"
+                            :items-per-page="5"
+                            class="elevation-1"
+                        ></v-data-table>
+                   </v-col>
+               </v-row>
+           </v-card>
+       </v-col>
+   </v-row>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            selectedItem: 0,
+            items: [
+                { text: 'My Files', icon: 'mdi-folder' },
+                { text: 'Shared with me', icon: 'mdi-account-multiple' },
+                { text: 'Starred', icon: 'mdi-star' },
+                { text: 'Recent', icon: 'mdi-history' },
+                { text: 'Offline', icon: 'mdi-check-circle' },
+                { text: 'Uploads', icon: 'mdi-upload' },
+                { text: 'Backups', icon: 'mdi-cloud-upload' },
+            ],
+
+             headers: [
+                {
+                text: 'Countries',
+                align: 'start',
+                sortable: false,
+                value: 'name',
+                },
+                { text: 'SalesRate', value: 'SalesRate' },
+                { text: 'Number Of Participant', value: 'Participant' },
+                { text: 'Famous Brand', value: 'Brand' },
+                { text: 'Year', value: 'year' },
+
+      ],
+      countries: [
+        {
+          name: 'America',
+          SalesRate: 159,
+          Participant: 6.0,
+          Brand: 24,
+          year: 2019,
+
+        },
+        {
+          name: 'Canada',
+          SalesRate: 237,
+          Participant: 9.0,
+          Brand: 37,
+          year: 2020,
+ 
+        },
+        {
+          name: 'Iran',
+          SalesRate: 262,
+          Participant: 16.0,
+          Brand: 23,
+          year: 2020,
+ 
+        },
+        {
+          name: 'Germany',
+          SalesRate: 305,
+          Participant: 3.7,
+          Brand: 67,
+          year: 2020,
+
+        },
+        {
+          name: 'Japn',
+          SalesRate: 356,
+          Participant: 16.0,
+          Brand: 49,
+          year: 2018,
+
+        },
+        {
+          name: 'China',
+          SalesRate: 375,
+          Participant: 0.0,
+          Brand: 94,
+          year: 2018,
+
+        },
+        {
+          name: 'India',
+          SalesRate: 392,
+          Participant: 0.2,
+          Brand: 98,
+          year:2019,
+
+        },
+        {
+          name: 'Italy',
+          SalesRate: 408,
+          Participant: 3.2,
+          Brand: 87,
+          year: 2020,
+
+        },
+        {
+          name: 'France',
+          SalesRate: 452,
+          Participant: 25.0,
+          Brand: 51,
+          year: 2018,
+
+        },
+        {
+          name: 'Poland',
+          SalesRate: 518,
+          Participant: 26.0,
+          Brand: 65,
+          year: 2020,
+
+        },
+      ],
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="about">
+    <!-- <h1>This is an about page</h1>
+    <button @click="increase(5)">increase</button>
+    <button @click="decrease()">decrease</button>
+    <p>{{counter}}</p>
+    <p v-once>{{counter}}</p>
+
+    <input type="text"
+     v-on:input="this.setName"
+     v-on:keyup.enter="ConfirmInput"/><br/>
+
+     <input type="text" v-model="name"/>
+    <p>your name: {{ ConfirmName }}</p>
+
+    <button @click="Reset()"> Rest Value </button>
+    
+    <form v-on:submit.prevent>
+      <input type="text"/>
+      <button :style="{borderColor : 'red'}">submit </button>
+    </form> -->
+
+    <div class="demo" :class="{active : boxASelected}" @click="boxSelected('A')">Hello this is a test </div>
+    
+    <div class="demo" :class="selectBox" @click="boxSelected('B')">Hello this is a test</div>
+
+    <div class="demo" :class="{active : boxASelected}" @click="boxSelected('C')">Hello this is a test</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      counter: 0,
+      name: '',
+      ConfirmName:'',
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    }
+  },
+  computed: {
+    selectBox(){
+      return {
+        active : this.boxASelected
+      };
+    }
+  },
+  
+  methods: {
+    boxSelected(box){
+      if (box === 'A'){
+        this.boxASelected = !this.boxASelected;
+      } else if (box === 'B'){
+        this.boxBSelected = !this.boxBSelected;
+      }else if (box === 'C'){
+        this.boxCSelected = !this.boxCSelected;
+      }
+
+    },
+
+    Reset(){
+      this.name = ''
+    },
+
+    ConfirmInput(){
+      this.ConfirmName = this.name
+    },
+
+    setName (event){
+      this.name = event.target.value;
+    },
+
+    increase (num){
+      this.counter = this.counter + num
+    },
+    decrease (){
+      this.counter--
+    }
+  },
+  watch: {
+    counter(value){
+      if (value > 50) {
+        const that = this;
+        setTimeout(function (){
+          that.counter = 0
+        },2000);
+        
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.demo {
+    width: calc(100% 32px);
+    height: 100px;
+    margin: 2px dashed #ccc;
+}
+
+.active {
+    border-color: rebeccapurple;
+    background-color: rgb(169, 169, 243);
+}
+</style>
